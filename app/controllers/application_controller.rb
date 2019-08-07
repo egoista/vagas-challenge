46aa909c245febee9efbab29afee522890f9bd08
Felipe Silveira <fsilveira@bionexo.com>
2019-08-07 18:14:02 -0300

    add pt-br locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def translate(message)
+    I18n.t('.' + message, scope: [controller_name, action_name])
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module VagasChallenge
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    I18n.available_locales = ['pt-BR']
+    config.i18n.default_locale = 'pt-BR'
   end
 end

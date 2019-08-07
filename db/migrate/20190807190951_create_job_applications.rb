41db6b262a2b849a8bd1c0bac61d1efb37a5003e
Felipe Silveira <fsilveira@bionexo.com>
2019-08-07 16:11:03 -0300

    create JobApplication model and migration

---
+++ db/migrate/20190807190951_create_job_applications.rb
@@ -0,0 +1,10 @@
+class CreateJobApplications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :job_applications do |t|
+      t.references :person, foreign_key: true
+      t.references :job_opportunity, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

a55e632867811aa7888d0b01b2ce96b228a8d78d
Felipe Silveira <fsilveira@bionexo.com>
2019-08-07 16:33:52 -0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :v1 do
+    resources :people, path: 'pessoas', only: [:create]
+
+    resources :job_applications, path: 'candidaturas', only: [:create]
+    
+    resources :job_opportunities, path: 'vagas', only: [:create] do
+      resource :job_applications, path: 'candidaturas', only: [] do
+        get 'ranking'
+      end
+    end
+  end
 end

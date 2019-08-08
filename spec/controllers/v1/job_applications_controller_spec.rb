8c039b690accce8f37930944fba5b82e0fa2bd45
Felipe Silveira <fsilveira@bionexo.com>
2019-08-08 16:45:20 -0300

    add JobApplicationController#ranking tests

--- spec/controllers/v1/job_applications_controller_spec.rb
+++ spec/controllers/v1/job_applications_controller_spec.rb
@@ -69,4 +69,37 @@ RSpec.describe V1::JobApplicationsController, type: :controller do
     end
   end
   
+  describe 'GET ranking' do
+    context 'when job_opportunity exists' do
+      let(:job_application) { create(:job_application) }
+      it "has a 200 status code" do
+        get :ranking, params: { job_opportunity_id: job_application.job_opportunity_id }
+
+        expect(response.status).to eq(200)
+      end
+
+      it "returns a json response with job_applications for opportunities" do
+        get :ranking, params: { job_opportunity_id: job_application.job_opportunity_id }
+
+        json = JSON.parse(response.body)
+        expect(json).to include_json([
+          {
+            "nome": job_application.person.name,
+            "profissao": job_application.person.occupation,
+            "localizacao": job_application.person.location.name,
+            "nivel": Person.levels[job_application.person.level],
+            "score": job_application.score
+          }
+        ])
+      end
+    end
+
+    context 'when job_opportunity does not exists' do
+      it "has a 404 status code" do
+        get :ranking, params: { job_opportunity_id: 1 }
+
+        expect(response.status).to eq(404)
+      end
+    end
+  end
 end
